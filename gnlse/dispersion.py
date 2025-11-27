6d736da34ab608234f1143203bb3a4d22417db68
Ellie Saga <elliesaga03@gmail.com>
2025-11-27 09:43:25 +0000

    switched from np factorial to math factorial

--- gnlse/dispersion.py
+++ gnlse/dispersion.py
@@ -6,6 +6,7 @@ frequency domain.
 
 """
 import numpy as np
+import math
 from scipy import interpolate
 
 from gnlse.common import c
@@ -67,7 +68,7 @@ class DispersionFiberFromTaylor(Dispersion):
         self.calc_loss()
         # Taylor series for subsequent derivatives
         # of constant propagation
-        B = sum(beta / np.math.factorial(i + 2) * V**(i + 2)
+        B = sum(beta / math.factorial(i + 2) * V**(i + 2)
                 for i, beta in enumerate(self.betas))
         L = 1j * B - self.alpha / 2
         return L
